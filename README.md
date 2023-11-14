c2e97b628219059a49e4292b3d7cb83b237a3102
Dmitry <babanov91@gmail.com>
2023-11-14 23:06:32 +0500

    init

---
+++ README.md
@@ -0,0 +1,36 @@
+# Fastapi Web Starter
+
+
+## Overview
+
+
+See requirements.txt for updates.
+
+```sh
+requests==2.27.1
+fastapi==0.72.0
+uvicorn==0.17.0
+python-dotenv==0.19.2
+aiofiles==0.8.0
+python-multipart==0.0.5
+jinja2==3.0.3
+Markdown==3.3.6
+pytest==6.2.5
+```
+
+## Installation & Usage
+
+```bash
+$ git clone git@github.com:shinokada/fastapi-web-starter.git
+$ cd fastapi-web-starter
+# install packages
+$ pip install -r requirements.txt
+# start the server
+$ uvicorn app.main:app --reload --port 8080
+```
+
+Visit [http://127.0.0.1:8080/](http://127.0.0.1:8080/).
+
+![Starting](./images/image-1.png)
+
+## Features
---
+++ app/main.py
@@ -0,0 +1,36 @@
+import uvicorn
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+
+from app.library.helpers import *
+from app.routers import twoforms, unsplash, accordion
+
+
+app = FastAPI()
+# print(dir)
+templates = Jinja2Templates(directory="templates")
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="app.static"), name="static")
+
+app.include_router(unsplash.router)
+app.include_router(twoforms.router)
+app.include_router(accordion.router)
+
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = openfile("home.md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def show_page(request: Request, page_name: str):
+    data = openfile(page_name+".md")
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+if __name__ == '__main__':
+    uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True)
